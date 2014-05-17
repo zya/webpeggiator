5844e9f7398573fd0a78a1c0c49b3d313dc8ff79
zya <ehsan.ziya@gmail.com>
2014-05-17 13:05:35 +0200

    Test

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var midi = null; //global midi object
+var peer = null;
+var conn = null;
 
 function midisuccess(access){
 
@@ -7,20 +9,42 @@ function midisuccess(access){
 	var inputs = midi.inputs();
 	var outputs = midi.outputs();
 
-	console.log(inputs);
-	console.log(outputs);
-
+	/*
 	inputs[0].onmidimessage = function(e){
 		console.log(e);
 	};
+	*/
 }
 
 function midierror(){
 	console.log('midi access failed');
 }
 
+//peer
+
+function peerOpen(id){
+	console.log('My peer Id is ' + id);
+}
+
+
 window.onload = function(){
 
 	navigator.requestMIDIAccess().then(midisuccess, midierror);
+
+	$('#create').click(function(){
+		var id = $('#customID').val();
+		peer = new Peer(id, {key: 'pa8gvlxhg0fkzkt9'});
+		peer.on('open', peerOpen);
+
+		peer.on('connection',function(conn){
+			console.log(conn);
+		});
+	});
+
+	$('#connect').click(function(){
+		peer = new Peer({key:'pa8gvlxhg0fkzkt9'});
+		peer.on('open',peerOpen);
+		conn = peer.conn('zya');
+	});
 
 };
